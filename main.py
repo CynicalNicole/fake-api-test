d012249dbfcb9f834a58dd00237f1c970ffacaa7
Nicole <nicole@cynicalpopcorn.me>
2021-02-22 15:16:40 +0000

    changes to the fake data loading and added more routes

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
 import flask
 import random
+import json
 from flask import request, jsonify
 
-companyData = [
-    {'id': 0, 'reference': 'AM1002', 'companyName': 'Testing', 'director': 'AA', 'tradingStatus': 'Trading', 'taxPlanning': 'Yes'},
-    {'id': 1, 'reference': 'AM1003', 'companyName': 'Testing1', 'director': 'AA', 'tradingStatus': 'Trading', 'taxPlanning': 'Yes'},
-    {'id': 2, 'reference': 'AM1004', 'companyName': 'Testing2', 'director': 'AA', 'tradingStatus': 'Trading', 'taxPlanning': 'Yes'},
-    {'id': 3, 'reference': 'AM1005', 'companyName': 'Testing3', 'director': 'AA', 'tradingStatus': 'Trading', 'taxPlanning': 'Yes'},
-    {'id': 4, 'reference': 'AM1006', 'companyName': 'Testing4', 'director': 'AA', 'tradingStatus': 'Trading', 'taxPlanning': 'Yes'},
-    {'id': 5, 'reference': 'AM1007', 'companyName': 'Testing5', 'director': 'AA', 'tradingStatus': 'Trading', 'taxPlanning': 'Yes'},
-    {'id': 6, 'reference': 'AM1008', 'companyName': 'Testing6', 'director': 'AA', 'tradingStatus': 'Trading', 'taxPlanning': 'Yes'},
-    {'id': 7, 'reference': 'AM1009', 'companyName': 'Testing7', 'director': 'AA', 'tradingStatus': 'Trading', 'taxPlanning': 'Yes'},
-    {'id': 8, 'reference': 'AM1010', 'companyName': 'Testing8', 'director': 'AA', 'tradingStatus': 'Trading', 'taxPlanning': 'Yes'},
-    {'id': 9, 'reference': 'AM1011', 'companyName': 'Testing9', 'director': 'AA', 'tradingStatus': 'Trading', 'taxPlanning': 'Yes'},
-    {'id': 10, 'reference': 'AM1012', 'companyName': 'Testing10', 'director': 'AA', 'tradingStatus': 'Trading', 'taxPlanning': 'Yes'},
-    {'id': 11, 'reference': 'AM1013', 'companyName': 'Testing11', 'director': 'AA', 'tradingStatus': 'Trading', 'taxPlanning': 'Yes'},
-    {'id': 12, 'reference': 'AM1014', 'companyName': 'Testing12', 'director': 'AA', 'tradingStatus': 'Trading', 'taxPlanning': 'Yes'},
-    {'id': 13, 'reference': 'AM1015', 'companyName': 'Testing13', 'director': 'AA', 'tradingStatus': 'Trading', 'taxPlanning': 'Yes'},
-    {'id': 14, 'reference': 'AM1016', 'companyName': 'Testing14', 'director': 'AA', 'tradingStatus': 'Trading', 'taxPlanning': 'Yes'},
-    {'id': 15, 'reference': 'AM1017', 'companyName': 'Testing15', 'director': 'AA', 'tradingStatus': 'Trading', 'taxPlanning': 'Yes'},
-    {'id': 16, 'reference': 'AM1018', 'companyName': 'Testing16', 'director': 'AA', 'tradingStatus': 'Trading', 'taxPlanning': 'Yes'}
-]
+with open('data.json') as json_data:
+    apiData = json.load(json_data)
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -41,6 +25,48 @@ def testendpoint():
 
     return jsonify(firstByField('companyId', id))
 
+@app.route('/getillustrationlist', methods=['GET'])
+def getIllustrationList(skip = None, take = None):
+    if 'skip' in request.args:
+        skip = int(request.args['skip'])
+
+    if 'take' in request.args:
+        take = int(request.args['take'])
+
+    if (skip != None):
+        end = skip + take
+        return jsonify(apiData["illustList"][skip:end])
+
+    return jsonify(apiData["illustList"])
+
+@app.route('/getinvoicelist', methods=['GET'])
+def getInvoiceList(skip = None, take = None):
+    if 'skip' in request.args:
+        skip = int(request.args['skip'])
+
+    if 'take' in request.args:
+        take = int(request.args['take'])
+
+    if (skip != None):
+        end = skip + take
+        return jsonify(apiData["invoiceList"][skip:end])
+
+    return jsonify(apiData["invoiceList"])
+
+@app.route('/getemployeelist', methods=['GET'])
+def getEmployeeList(skip = None, take = None):
+    if 'skip' in request.args:
+        skip = int(request.args['skip'])
+
+    if 'take' in request.args:
+        take = int(request.args['take'])
+
+    if (skip != None):
+        end = skip + take
+        return jsonify(apiData["employeeList"][skip:end])
+
+    return jsonify(apiData["employeeList"])
+
 @app.route('/getcompanylist', methods=['GET'])
 def getCompanyList(skip = None, take = None):
     if 'skip' in request.args:
@@ -51,12 +77,12 @@ def getCompanyList(skip = None, take = None):
     
     if (skip != None):
         end = skip + take
-        return jsonify(companyData[skip:end])
+        return jsonify(apiData["companyList"][skip:end])
 
-    return jsonify(companyData)
+    return jsonify(apiData["companyList"])
 
 def firstByField(field, value):
-    for company in companyData:
+    for company in apiData["companyList"]:
         if company[field] == value:
             return company
 
